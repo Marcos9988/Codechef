f1bcd9b584577937df0cd1fe633b3be809adaca7
Ashish mishra <52116179+Marcos9988@users.noreply.github.com>
2020-04-27 15:57:26 +0530

    Add files via upload

---
+++ April Cookoff 2020/MATBREAK.cpp
@@ -0,0 +1,100 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long 
+#define fi first
+#define se second
+#define all(x) (x).begin(), (x).end()
+#define allrev(x) (x).rbegin(), (x).end()
+#define pb push_back
+#define pf push_front
+#define fori(i, a, n) for(int i = a; i < n; ++i)
+#define MAX(x) (*max_element(all(x)))
+#define MIN(x) (*min_element(all(x)))
+#define sortarr(a,n) sort(a, a+n)
+#define sortvec(V) sort(all(V))
+
+typedef vector<int> V;
+typedef pair<int , int> pairi;
+typedef set<int> seti;
+typedef deque<int> deqi;
+
+const int MOD = 1e9+7;
+const int INF = 1e9+8;
+const double pi = 3.14159265359;
+
+int gcd(int a, int b)
+{
+    return b == 0 ? a : gcd(b, a % b);
+}
+int fac(int x)
+{
+    if(x == 0)  return 1;
+    return (x * fac(x - 1)); 
+}
+template <class T> 
+T gcd(T x, T y)     // template function to find gcd of two numbers
+{
+    T gc;
+    for(T i = 1; i <= x && i <= y; i++)
+    {
+        if(x%i == 0 && y % i == 0)
+            gc = i;
+    }
+    return gc;
+}
+template <class T>
+T fac(T n)      // template function to find factorial of a number
+{ 
+    T res = 1, i; 
+    for (i = 2; i <= n; i++) 
+        res *= i; 
+    return res; 
+}
+
+template<class T>
+T lcm(T a, T b)     //template function to find lcm of two numbers 
+{
+    return (a * b)/gcd(a,b);
+}
+
+string tostring(int a)      //converting integer into a string
+{
+    string ans;
+    while(a){
+        ans += char(a % 10 + '0');
+        a /= 10;
+    }
+    return ans;
+}
+ll power(ll a, ll b){
+    if(b == 0)  return 1;
+    if(b == 1)  return a;
+    else{
+        ll t = power(a, b/2);
+        if(b % 2 == 0)  return ((t%MOD) * (t%MOD))%MOD;
+        else return ((((t%MOD)*(t%MOD))%MOD)*a%MOD)%MOD;
+    }
+}
+void solve()
+{
+    ll n, a, ans = 0, y;
+    cin >> n >> a;
+    fori(i, 0, n){
+        y = power(a, 2*i + 1);
+        y = y % MOD;
+        ans = (ans % MOD + y % MOD) % MOD;
+        a = ((a % MOD)*(y % MOD)) % MOD;
+    }
+    cout<<ans<<endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t;
+    cin >>t;
+    while(t--)
+    solve();
+}
